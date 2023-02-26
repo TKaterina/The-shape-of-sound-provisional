19c07b4b96b057a63ad8840cb967265b35a6bbd8
Katerina Tampouraki <ktampouraki@gmail.com>
2023-02-26 17:31:27 +0000

    Update comments and add possible filenames when calling

--- PycharmProjects/helloWorld/masked_sift.py
+++ PycharmProjects/helloWorld/masked_sift.py
@@ -3,14 +3,14 @@ import librosa
 
 
 def masked_sift_1965(filename):
-    # this mask works for the cello and viola, although the viola decomposition is not perfect.
-    # Can't tell if the previous mask works better for the viola, both look quite similar.
+    # filenames when calling:cello_C4_1_fortissimo_arco-normal
+    #                        viola_C4_1_fortissimo_arco-normal
+    # Can't tell if the previous custom mask works better for the viola since both look quite similar
 
     y, sr = librosa.load('C://Users/ktamp/OneDrive/Desktop/TheShapeofSound/mp3Gallery/' +
                          filename + '.mp3', offset=0.25, duration=0.5)
 
-    # used a slightly longer duration to make sure that that weird wave thing of the viola is showing
-    # (it seemed important to include)
+    # slightly longer duration to make sure that that weird wave thing of the viola is visible
 
     mask_freq_1 = [1965/sr, 778/sr, 259/sr, 194/sr]
     imf, mask_freqs = emd.sift.mask_sift(y, mask_freqs=mask_freq_1, ret_mask_freq=True, max_imfs=4)
@@ -21,13 +21,13 @@ def masked_sift_1965(filename):
 
 
 def masked_sift_2530(filename):
-
-    # this mask frequency seems to work for the sax, trombone and double-bass.
-    # originally the sax has a mask_freq of 2799 and the trombone 3169.
-    # for the sax the decomposition is the same as when a custom mask was used, but for the trombone
-    # the decomposition is different compared to before. Maybe this version of the decomposition is better because
-    # imfs 2, 3, 4 look better this way.
-    # surprisingly it also works for the violin. There seems to be some mode-mixing between imf3 and 4 but
+    # filenames when calling:double-bass_C4_1_fortissimo_arco-normal
+    #                        violin_C4_1_fortissimo_arco-normal
+    #                        saxophone_C4_1_fortissimo_normal
+    #                        trombone_C4_1_fortissimo_normal
+    # Cannot tell if for the trombone the decomposition is better than when using the custom mask,
+    # but imfs 2, 3, 4, seem a bit better.
+    # For the violin there seems to be some mode-mixing between imf3 and 4 but
     # that happens with the custom mask. Can't get rig of the mixing completely.
     y, sr = librosa.load('C://Users/ktamp/OneDrive/Desktop/TheShapeofSound/mp3Gallery/' +
                          filename + '.mp3', offset=0.25, duration=0.25)
@@ -41,9 +41,9 @@ def masked_sift_2530(filename):
 
 
 def masked_sift_1939(filename):
-
-    # this one works for both guitar and banjo and is better than the previous versions of the custom masks
-    # Not perfect but pretty good.
+    # filenames when calling:guitar_C4_very - long_forte_normal
+    #                        banjo_C4_very-long_forte_normal
+    # this one works better than the previous versions of the custom masks, probably not perfect still.
 
     y, sr = librosa.load('C://Users/ktamp/OneDrive/Desktop/TheShapeofSound/mp3Gallery/' +
                          filename + '.mp3', offset=0.25, duration=0.25)
@@ -57,14 +57,15 @@ def masked_sift_1939(filename):
 
 
 def masked_sift_1322(filename):
-
-    # this one works for the clarinet, bass-clarinet and flute.
+    # filenames when calling:clarinet_C4_1_fortissimo_normal
+    #                        flute_C4_1_forte_normal
+    #                        bass-clarinet_C4_1_fortissimo_normal
     # it's possible that the decomposition is not as good for the clarinet, but it's not bad I think.
 
     y, sr = librosa.load('C://Users/ktamp/OneDrive/Desktop/TheShapeofSound/mp3Gallery/' +
                          filename + '.mp3', offset=0.75, duration=0.25)
 
-    # chose a later offset so that both of the weird wave things of the clarinet are visible.
+    # later offset so that both of the weird wave things of the clarinet are visible.
 
     mask_freq_4 = 1322/sr
     imf, mask_freqs = emd.sift.mask_sift(y, mask_freqs=mask_freq_4, ret_mask_freq=True, max_imfs=5)
@@ -89,6 +90,6 @@ def iterated_masked_sift(filename):
     return fig_mask, mask_freqs
 
 
-# for the tuba the simple sift continues to be the best decomposition.
+# for the tuba the simple sift seems to be the best decomposition
 
 
