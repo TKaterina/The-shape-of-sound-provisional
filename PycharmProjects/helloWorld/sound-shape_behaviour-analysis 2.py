bdc431cdd2ff2a37055df44a857b11a3909313ed
Katerina <katerinatampouraki01@gmail.com>
2023-07-01 18:39:24 +0100

    Add covariance calculation

--- PycharmProjects/helloWorld/sound-shape_behaviour-analysis 2.py
+++ PycharmProjects/helloWorld/sound-shape_behaviour-analysis 2.py
@@ -6,6 +6,9 @@ import numpy as np
 df01 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\S001_Roughness._2023_Jun_29_1205.csv")
 df02 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\s002_Roughness._2023_Jun_29_1409.csv")
 df03 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\S003_Roughness._2023_Jun_29_1603.csv")
+df04 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\s004_Roughness._2023_Jun_30_1239.csv")
+df05 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\s005_Roughness._2023_Jun_30_1400.csv")
+df06 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\S006_Roughness._2023_Jun_30_1504.csv")
 
 
 def prep(data_frame):
@@ -27,9 +30,12 @@ def prep(data_frame):
 xtab1, rt1, am = prep(df01)
 xtab2, rt2, _ = prep(df02)
 xtab3, rt3, _ = prep(df03)
+xtab4, rt4, _ = prep(df04)
+xtab5, rt5, _ = prep(df05)
+xtab6, rt6, _ = prep(df06)
 
 
-rt = pd.concat([rt1, rt2, rt3])
+rt = pd.concat([rt1, rt2, rt3, rt4, rt5, rt6])
 
 M = np.mean(rt.values)
 SD = np.std(rt.values)
@@ -47,10 +53,16 @@ def remove_outliers(xtab,upper,lower):
     return ratings,xtab,
 
 
-ratings = [remove_outliers(xtab1, upper_bound, lower_bound)[0], remove_outliers(xtab2, upper_bound, lower_bound)[0], remove_outliers(xtab3, upper_bound, lower_bound)[0]]
+ratings = [remove_outliers(xtab1, upper_bound, lower_bound)[0],
+           remove_outliers(xtab2, upper_bound, lower_bound)[0],
+           remove_outliers(xtab3, upper_bound, lower_bound)[0],
+           remove_outliers(xtab4, upper_bound, lower_bound)[0],
+           remove_outliers(xtab5, upper_bound, lower_bound)[0],
+           remove_outliers(xtab6, upper_bound, lower_bound)[0]]
 
 output = np.zeros((len(ratings), 1))
 result = np.zeros((len(ratings[0]), 3))
+standard_deviation = np.zeros((len(ratings[0]), 3))
 
 for k in np.arange(0,3):
     for j in range(len(ratings[0])):
@@ -61,6 +73,8 @@ for k in np.arange(0,3):
                 output[i] = 'NaN'
 
         result[j][k] = np.nanmean(output)
+        standard_deviation[j][k] = np.nanstd(output)
+
 
 
 plt.figure()
@@ -74,5 +88,16 @@ for tag in ['top', 'right']:
     plt.gca().spines[tag].set_visible(False)
 plt.title('Percept x AM freq')
 
-
+# temporary calculation of covariance for each modulator frequency and percept.
+covariance = standard_deviation/result
+plt.figure()
+plt.plot(np.arange(7), covariance, lw=2)
+plt.xticks(np.arange(7), am)
+plt.xlabel('Amplitude Modulation Frequenacy (Hz)')
+plt.ylabel('Covariance of ratings')
+plt.legend(['Pitch', 'Roughness', 'Tremolo'])
+plt.grid(True)
+for tag in ['top', 'right']:
+    plt.gca().spines[tag].set_visible(False)
+plt.title('Percept x AM freq')
 
