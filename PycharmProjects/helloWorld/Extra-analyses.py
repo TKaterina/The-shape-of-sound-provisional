1a577cf91a10b94a701a219e26004e180f1658f5
Katerina <katerinatampouraki01@gmail.com>
2023-07-01 18:41:05 +0100

    Calculate Percept x AM per carrier frequency

--- PycharmProjects/helloWorld/Extra-analyses.py
+++ PycharmProjects/helloWorld/Extra-analyses.py
@@ -6,6 +6,9 @@ import numpy as np
 df01 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\S001_Roughness._2023_Jun_29_1205.csv")
 df02 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\s002_Roughness._2023_Jun_29_1409.csv")
 df03 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\S003_Roughness._2023_Jun_29_1603.csv")
+df04 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\s004_Roughness._2023_Jun_30_1239.csv")
+df05 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\s005_Roughness._2023_Jun_30_1400.csv")
+df06 = pd.read_csv("C:\\Users\ktamp\OneDrive\Desktop\The-shape-of-sound-provisional-master\data\S006_Roughness._2023_Jun_30_1504.csv")
 
 
 def prep(data_frame):
@@ -15,23 +18,65 @@ def prep(data_frame):
 
     data_frame = data_frame[['carrier', 'am', 'question', 'slider_3.response', 'slider_3.rt']]
 
-    xtab = data_frame.pivot_table(index=['question', 'am'])
+    xtab = data_frame.pivot_table(index=['question', 'am', 'carrier'])
 
-    response_time = data_frame['slider_3.rt'].to_frame()
+    ratings = xtab['slider_3.response'].values.reshape(3, 7, 3).T
 
-    ratings = xtab['slider_3.response'].values.reshape(3, 7).T
     amp = np.unique(data_frame['am'].values)
+    carr = np.unique(data_frame['carrier'].values)
 
-    return xtab, response_time, amp, ratings
+    return xtab, ratings, amp, carr
 
 
-xtab1, rt1, am, _ = prep(df01)
-xtab2, rt2, _, _ = prep(df02)
-xtab3, rt3, _, _ = prep(df03)
+xtab1 = prep(df01)[0]
+xtab2 = prep(df02)[0]
+xtab3 = prep(df03)[0]
+xtab4 = prep(df04)[0]
+xtab5 = prep(df05)[0]
+xtab6 = prep(df06)[0]
 
-ratings = [prep(df01)[3], prep(df02)[3], prep(df03)[3]]
+ratings = [prep(df01)[1], prep(df02)[1], prep(df03)[1], prep(df04)[1], prep(df05)[1], prep(df06)[1]]
+
+am = prep(df01)[2]
+carrier = prep(df01)[3]
+
+output = np.zeros((len(ratings), 1))
+result = np.zeros([len(carrier), len(am), 3])
+standard_deviation = np.zeros([len(carrier), len(am), 3])
+
+for m in np.arange(0,3):
+    for k in range(len(carrier)):
+        for j in range(len(am)):
+            for i in range(len(ratings)):
+                if ratings[i][k][j][m] != 'Outlier':
+                    output[i] = ratings[i][k][j][m]
+                else:
+                    output[i] = 'NaN'
+
+            result[k][j][m] = np.nanmean(output)
+            standard_deviation[k][j] = np.nanstd(output)
+
+
+carrier_title = ['440 Hz carrier', '700 Hz carrier', '1000 Hz carrier']
+plt.figure()
+for i in np.arange(0,3):
+    plt.subplot(1,3,i+1)
+    plt.plot(np.arange(7), result[i], lw=2)
+    plt.xticks(np.arange(7), am)
+    plt.xlabel('Amplitude Modulation Frequenacy (Hz)')
+    plt.ylabel('Perceived Difference Rating')
+    plt.legend(['Pitch', 'Roughness', 'Tremolo'])
+    plt.grid(True)
+    for tag in ['top', 'right']:
+        plt.gca().spines[tag].set_visible(False)
+    plt.title(carrier_title[i])
+
+
 
 plt.plot(ratings[0], linestyle='solid')
 plt.plot(ratings[1], linestyle='dashed')
 plt.plot(ratings[2], linestyle='dotted')
+plt.plot(ratings[3], linestyle='solid')
+plt.plot(ratings[4], linestyle='dashed')
+plt.plot(ratings[5], linestyle='dotted')
 
