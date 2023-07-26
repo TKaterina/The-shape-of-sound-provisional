be7e47f705c6e78e7b4df030dbe2752da0c238c1
Katerina <katerinatampouraki01@gmail.com>
2023-07-26 15:18:33 +0100

    Include FFT and weighted IF average in analysis

--- PycharmProjects/helloWorld/experiment_1_signal_processing.py
+++ PycharmProjects/helloWorld/experiment_1_signal_processing.py
@@ -1,7 +1,8 @@
 import numpy as np
 import emd
 import matplotlib.pyplot as plt
-
+from scipy.fft import fft, fftfreq
+# from scipy.io.wavfile import write
 
 mod = 1
 sr = 48000
@@ -9,9 +10,7 @@ time = np.linspace(0, 1, sr)
 
 carrier = [440, 700, 1000]
 am = [5, 10, 20, 40, 80, 160, 320]
-mask_freq_1 = 450/sr
-mask_freq_2 = 724/sr
-mask_freq_3 = 1014/sr
+
 
 for i in np.arange(0,3):
     for j in np.arange(0,7):
@@ -19,22 +18,27 @@ for i in np.arange(0,3):
         amp = np.cos(2*np.pi*am[j]*time)
         tone = carrier_tone * (1 + mod * amp)
 
-        imf, mask_freqs = emd.sift.mask_sift(tone, mask_freqs=[(carrier[i]+am[i])/sr, (carrier[i]-am[i])/sr],
-                                             ret_mask_freq=True, max_imfs=4)
-        fig_mask = emd.plotting.plot_imfs(imf[:sr, :])
-
-
-        # other sifting options:
-
-        # imf, mask_freqs = emd.sift.mask_sift(tone, mask_freqs=carrier[i]/sr, ret_mask_freq=True, max_imfs=4)
-        # fig_mask = emd.plotting.plot_imfs(imf[:sr, :])
-
-        # imf, mask_freqs = emd.sift.iterated_mask_sift(tone, sample_rate=sr, w_method='amplitude', max_imfs=5,
-        #                                             ret_mask_freq=True)
-        #
-        # fig_mask = emd.plotting.plot_imfs(imf[:sr, :])
-
-        # imf = emd.sift.sift(tone, imf_opts={'sd_thresh': 1e-3})
-        # fig = emd.plotting.plot_imfs(imf[:sr, :])
-
+        imf = emd.sift.sift(tone, imf_opts={'sd_thresh': 1e-3})
+        IP, IF, IA = emd.spectra.frequency_transform(imf, sr, 'hilbert', smooth_phase=441)
+        fig = emd.plotting.plot_imfs(imf[:5000, :])
         plt.show()
+
+        N = sr * 1
+        yf = fft(imf[:sr, 0])
+        xf = fftfreq(N, 1 / sr)
+
+        plt.plot(xf, np.abs(yf))
+        plt.show()
+
+        print(np.average(IF[:, 0], weights=IA[:, 0]))
+
+
+# extracts first imf
+# imf[:sample_rate, 0]
+
+
+# fourier = np.fft.fft(imf[:sr, 0])
+# n = signal.size
+# timestep = 0.1
+# freq = np.fft.fftfreq(n, d=timestep)
+# freq
