f17c32df3ced097c7dfa2e7fbf87dd8e027a5e13
Katerina <katerinatampouraki01@gmail.com>
2023-07-26 15:25:14 +0100

    Calculate 63 paired samples t-tests for each percept comparison, modulator and carrier

---
+++ PycharmProjects/helloWorld/experiment_1_t-test_per_carrier.py
@@ -0,0 +1,285 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+from scipy import stats
+
+# create array of excel file names
+names = ['S001_Roughness._2023_Jun_29_1205.csv',
+         #'s002_Roughness._2023_Jun_29_1409.csv',
+         'S003_Roughness._2023_Jun_29_1603.csv',
+         's004_Roughness._2023_Jun_30_1239.csv',
+         's005_Roughness._2023_Jun_30_1400.csv',
+         'S006_Roughness._2023_Jun_30_1504.csv',
+         's007_Roughness._2023_Jul_03_1305.csv',
+         's008_Roughness._2023_Jul_03_1505.csv',
+         's009_Roughness._2023_Jul_04_1313.csv',
+         'S010_Roughness._2023_Jul_04_1433.csv',
+         's011_Roughness._2023_Jul_05_1215.csv',
+         's012_Roughness._2023_Jul_05_1304.csv',
+         's013_Roughness._2023_Jul_05_1408.csv',
+         's014_Roughness._2023_Jul_05_1515.csv',
+         'S015_Roughness._2023_Jul_07_1210.csv',
+         's016_Roughness._2023_Jul_07_1513.csv',
+         's017_Roughness._2023_Jul_11_1207.csv',
+         's018_Roughness._2023_Jul_11_1308.csv',
+         'S019_Roughness._2023_Jul_11_1622.csv',
+         'S020_Roughness._2023_Jul_12_1208.csv',
+         's021_Roughness._2023_Jul_12_1607.csv']
+
+
+ratings440 = []
+ratings700 = []
+ratings1000 = []
+am = []
+response_time = []
+xtab_collection_440 = {}
+xtab_collection_700 = {}
+xtab_collection_1000 = {}
+
+
+for ii in range(len(names)):
+
+    csv_files = csv_file = 'C://Users/ktamp/OneDrive/Desktop/The-shape-of-sound-provisional-master/data/' + names[ii]
+    df = pd.read_csv(csv_files)
+
+    drops = np.isnan(df['slider_3.response'].values) == False
+    df = df[drops]
+    df = df[['carrier', 'am', 'question', 'slider_3.response', 'slider_3.rt']]
+
+    drops440 = df['carrier'].values == 440
+    drops700 = df['carrier'].values == 700
+    drops1000 = df['carrier'].values == 1000
+
+    df440 = df[drops440]
+    df700 = df[drops700]
+    df1000 = df[drops1000]
+
+    xtab440 = df440.pivot_table(index=['question', 'am'])
+    xtab700 = df700.pivot_table(index=['question', 'am'])
+    xtab1000 = df1000.pivot_table(index=['question', 'am'])
+
+    xtab_collection_440[ii] = xtab440
+    xtab_collection_700[ii] = xtab700
+    xtab_collection_1000[ii] = xtab1000
+
+    response_time.append(df['slider_3.rt'].to_frame())
+
+    am.append(np.unique(df['am'].values))
+    ratings440.append(xtab440['slider_3.response'].values.reshape(3, 7).T)
+    ratings700.append(xtab700['slider_3.response'].values.reshape(3, 7).T)
+    ratings1000.append(xtab1000['slider_3.response'].values.reshape(3, 7).T)
+
+
+rts = response_time[0]
+for i in range(len(names)-1):
+    rts = np.concatenate((rts, response_time[i+1]))
+
+
+M = np.mean(rts)
+SD = np.std(rts)
+lower_bound = M - 2*SD
+upper_bound = M + 2*SD
+
+# for ii in range(len(xtab_collection_440)):
+#     drops = ((xtab_collection_440[ii]['slider_3.rt'].values > upper_bound) | (xtab_collection_440[ii]['slider_3.rt'].values < lower_bound))
+#     xtab_collection_440[ii][drops == True] = np.nan
+#
+#     ratings440.append(xtab_collection_440[ii]['slider_3.response'].values.reshape(3, 7).T)
+#
+#     drops = ((xtab_collection_700[ii]['slider_3.rt'].values > upper_bound) | (xtab_collection_700[ii]['slider_3.rt'].values < lower_bound))
+#     xtab_collection_700[ii][drops == True] = np.nan
+#
+#     ratings700.append(xtab_collection_700[ii]['slider_3.response'].values.reshape(3, 7).T)
+#
+#     drops = ((xtab_collection_1000[ii]['slider_3.rt'].values > upper_bound) | (xtab_collection_1000[ii]['slider_3.rt'].values < lower_bound))
+#     xtab_collection_1000[ii][drops == True] = np.nan
+#
+#     ratings1000.append(xtab_collection_1000[ii]['slider_3.response'].values.reshape(3, 7).T)
+
+# Paired-sample t-test
+# could do a comparison for all percpets between carriers but that's not very informative
+ratings_coll = [ratings440, ratings700, ratings1000]
+
+# ppts x am x question
+for ii in np.arange(0,3):
+
+    ratings = np.concatenate([rt[None, :, :] for rt in ratings_coll[ii]], axis=0)
+
+    # comparing tremolo and roughness here
+    A = ratings[:, :, 0]
+    B = ratings[:, :, 1]
+    C = ratings[:, :, 2]
+
+
+    nperms = 1500
+    nulls1 = np.zeros((nperms,))
+    nulls2 = np.zeros((nperms,))
+    nulls3 = np.zeros((nperms,))
+
+    ratings_t1, ratings_p1 = stats.ttest_rel(A, B, axis=0) # pitch-roughness
+    ratings_t2, ratings_p2 = stats.ttest_rel(A, C, axis=0)
+    ratings_t3, ratings_p3 = stats.ttest_rel(B, C, axis=0)
+    nulls1[0] = ratings_t1.max()  # First null is the same as the observed data
+    nulls2[0] = ratings_t2.max()
+    nulls3[0] = ratings_t3.max()
+
+    # Randomly swap the conditions for half the data at each null (actually not
+    # always half the data, but each participant has a 0.5 probability of being
+    # swapped).
+    for ii in range(1, nperms):
+        swaps = np.random.choice([0, 1], 20)
+        perm_A = A.copy()
+        perm_B = B.copy()
+        for jj in range(len(swaps)):
+            if swaps[jj] == 1:
+                perm_A[jj, :] = B[jj, :]
+                perm_B[jj, :] = A[jj, :]
+        t, p = stats.ttest_ind(perm_A, perm_B, axis=0)
+        nulls1[ii] = t.max()
+
+    thresh1 = np.percentile(nulls1, [95, 99, 99.9])
+
+    for ii in range(1, nperms):
+        swaps = np.random.choice([0, 1], 20)
+        perm_A = A.copy()
+        perm_C = C.copy()
+        for jj in range(len(swaps)):
+            if swaps[jj] == 1:
+                perm_A[jj, :] = C[jj, :]
+                perm_C[jj, :] = A[jj, :]
+        t, p = stats.ttest_ind(perm_A, perm_C, axis=0)
+        nulls2[ii] = t.max()
+
+    thresh2 = np.percentile(nulls2, [95, 99, 99.9])
+
+    for ii in range(1, nperms):
+        swaps = np.random.choice([0, 1], 20)
+        perm_B = B.copy()
+        perm_C = C.copy()
+        for jj in range(len(swaps)):
+            if swaps[jj] == 1:
+                perm_B[jj, :] = C[jj, :]
+                perm_C[jj, :] = B[jj, :]
+        t, p = stats.ttest_ind(perm_B, perm_C, axis=0)
+        nulls3[ii] = t.max()
+
+    thresh3 = np.percentile(nulls3, [95, 99, 99.9])
+
+    plt.figure()
+    plt.subplot(121)
+    plt.plot((thresh1[0], thresh1[0]), (0, 250), 'k')
+    plt.plot((thresh1[1], thresh1[1]), (0, 250), 'k--')
+    plt.plot((thresh1[2], thresh1[2]), (0, 250), 'k:')
+    plt.legend(['p=0.05', 'p=0.01', 'p=0.001'])
+    plt.hist(nulls1, 64, color='b')
+    plt.xlabel('t-value')
+    plt.ylabel('number of occurences')
+    plt.title('Pitch-Roughness')
+
+
+    plt.subplot(122)
+    plt.plot(ratings_t1, color='b')
+    plt.plot((0, 6), (thresh1[0], thresh1[0]), 'k')
+    plt.plot((0, 6), (thresh1[1], thresh1[1]), 'k--')
+    plt.plot((0, 6), (thresh1[2], thresh1[2]), 'k:')
+    plt.legend(['Pitch', 'Roughness', 'Tremolo','p=0.05', 'p=0.01', 'p=0.001'])
+    plt.plot((0, 6), (-thresh1[0], -thresh1[0]), 'k')
+    plt.plot((0, 6), (-thresh1[1], -thresh1[1]), 'k--')
+    plt.plot((0, 6), (-thresh1[2], -thresh1[2]), 'k:')
+    plt.xticks(np.arange(7), am[0])
+    plt.xlabel('Amplitude Modulation Frequenacy (Hz)')
+    plt.ylabel('t-value')
+
+    plt.figure()
+    plt.subplot(121)
+    plt.plot((thresh2[0], thresh2[0]), (0, 250), 'k')
+    plt.plot((thresh2[1], thresh2[1]), (0, 250), 'k--')
+    plt.plot((thresh2[2], thresh2[2]), (0, 250), 'k:')
+    plt.legend(['p=0.05', 'p=0.01', 'p=0.001'])
+    plt.hist(nulls2, 64, color='r')
+    plt.xlabel('t-value')
+    plt.ylabel('number of occurences')
+    plt.title('Tremolo-Pitch')
+
+    plt.subplot(122)
+    plt.plot(ratings_t2, color='r')
+    plt.plot((0, 6), (thresh2[0], thresh2[0]), 'k')
+    plt.plot((0, 6), (thresh2[1], thresh2[1]), 'k--')
+    plt.plot((0, 6), (thresh2[2], thresh2[2]), 'k:')
+    plt.legend(['Pitch','Roughness', 'Tremolo', 'p=0.05', 'p=0.01', 'p=0.001'])
+    plt.plot((0, 6), (-thresh2[0], -thresh2[0]), 'k')
+    plt.plot((0, 6), (-thresh2[1], -thresh2[1]), 'k--')
+    plt.plot((0, 6), (-thresh2[2], -thresh2[2]), 'k:')
+    plt.xticks(np.arange(7), am[0])
+    plt.xlabel('Amplitude Modulation Frequency (Hz)')
+    plt.ylabel('t-value')
+
+    plt.figure()
+    plt.subplot(121)
+    plt.plot((thresh3[0], thresh3[0]), (0, 250), 'k')
+    plt.plot((thresh3[1], thresh3[1]), (0, 250), 'k--')
+    plt.plot((thresh3[2], thresh3[2]), (0, 250), 'k:')
+    plt.legend(['p=0.05', 'p=0.01', 'p=0.001'])
+    plt.hist(nulls3, 64, color='g')
+    plt.xlabel('t-value')
+    plt.ylabel('number of occurences')
+    plt.title('Roughness-Tremolo')
+
+    plt.subplot(122)
+    plt.plot(ratings_t3, color='g')
+    plt.plot((0, 6), (thresh3[0], thresh3[0]), 'k')
+    plt.plot((0, 6), (thresh3[1], thresh3[1]), 'k--')
+    plt.plot((0, 6), (thresh3[2], thresh3[2]), 'k:')
+    plt.legend(['Pitch','Roughness','Tremolo', 'p=0.05', 'p=0.01', 'p=0.001'])
+    plt.plot((0, 6), (-thresh3[0], -thresh3[0]), 'k')
+    plt.plot((0, 6), (-thresh3[1], -thresh3[1]), 'k--')
+    plt.plot((0, 6), (-thresh3[2], -thresh3[2]), 'k:')
+    plt.xticks(np.arange(7), am[0])
+    plt.xlabel('Amplitude Modulation Frequenacy (Hz)')
+    plt.ylabel('t-value')
+    plt.suptitle('permutation paired t-tests for difference between percepts')
+
+    # # ppts x am x question
+    # # ratings = np.concatenate([rt[None, :, :] for rt in ratings], axis=0)
+    # ratings = np.concatenate([rt[None, :, :] for rt in ratings_coll[ii]], axis=0)
+    # # ratings = ratings[~np.isnan(ratings)]
+    #
+    # nperms = 1500
+    # nulls = np.zeros((nperms,))
+    #
+    # ratings_t, ratings_p = stats.ttest_1samp(ratings, 50, axis=0)
+    # nulls[0] = ratings_t.max()  # First null is the same as the observed data
+    #
+    # for ii in range(1, nperms):
+    #     null_ratings = np.random.choice([1, -1], (20, 1, 1)) * ratings
+    #     t, p = stats.ttest_1samp(null_ratings, 50, axis=0)
+    #     nulls[ii] = t.max()
+    #
+    # thresh = np.percentile(nulls, [95, 99, 99.9])
+    #
+    # plt.figure()
+    # plt.subplot(121)
+    # plt.plot((thresh[0], thresh[0]), (0, 250), 'k')
+    # plt.plot((thresh[1], thresh[1]), (0, 250), 'k--')
+    # plt.plot((thresh[2], thresh[2]), (0, 250), 'k:')
+    # plt.legend(['p=0.05', 'p=0.01', 'p=0.001'])
+    # plt.hist(nulls, 64)
+    # plt.xlabel('t-value')
+    # plt.ylabel('number of occurrences')
+    #
+    # plt.subplot(122)
+    # plt.plot(ratings_t)
+    # plt.plot((0, 6), (thresh[0], thresh[0]), 'k')
+    # plt.plot((0, 6), (thresh[1], thresh[1]), 'k--')
+    # plt.plot((0, 6), (thresh[2], thresh[2]), 'k:')
+    # plt.legend(['Tremolo', 'Roughness', 'Pitch', 'p=0.05', 'p=0.01', 'p=0.001'])
+    # plt.xticks(np.arange(7), am[0])
+    # plt.xlabel('Amplitude Modulation Frequency (Hz)')
+    # plt.ylabel('t-value')
+    #
+    # plt.suptitle('permutation t-tests for difference from zero')
+
+
+
+
